8dfa8612ad3843d2a2349569c0e4446434974728
samwoodson <samuelwoodson@gmail.com>
2016-04-01 21:02:26 +0000

    first oop version

--- 2pgame.rb
+++ 2pgame.rb
@@ -1,23 +1,21 @@
-require 'byebug'
-require 'colorize'
-@setnameflag = false
+require './players'
+
 LIVES = 3
 OPERATIONS = [:+, :*, :-]
 
-@players = [
-  {
-    id: 1,
-    lives: LIVES,
-    score: 0,
-    name: ''
-  },
-  {
-    id: 2,
-    lives: LIVES,
-    score: 0,
-    name: ''
-  }
-]
+def initialize_players
+  @players = [
+      Player.new("Player 1", 0),
+      Player.new("Player 2", 1)
+  ]
+end
+
+def set_names
+  puts "Player 1 please enter your name:"
+  @players[0].name = gets.chomp
+  puts "Player 2 please enter your name:"
+  @players[1].name = gets.chomp
+end
 
 def generate_question
   @sign = OPERATIONS[rand(0..2)]
@@ -26,8 +24,8 @@ def generate_question
   @answer = @num1.send(@sign, @num2)
 end
 
-def prompt(player_num)
-  puts "\n#{@players[player_num - 1][:name]}: What is #{@num1} #{@sign} #{@num2}?"
+def prompt(name)
+  puts "\n#{name}: What is #{@num1} #{@sign} #{@num2}?"
   @response = gets.chomp.to_i
 end
 
@@ -35,52 +33,44 @@ def verify
   @answer == @response
 end
 
-def scores(player_key)
+def scores(player)
     if verify
-      @players[player_key - 1][:score] += 1
+      player.gain_point
       puts "\e[32m"
       puts "Correct!"
       puts "\e[0m"
     else 
-        @players[player_key - 1][:lives] -= 1
-        if @players[player_key - 1][:lives] > 1
+        player.lose_life
+        if player.lives > 1
         puts "\e[31m"
-        puts "Wrong! #{@players[player_key - 1][:name]} you have #{@players[player_key - 1][:lives]} lives left!"
+        puts "Wrong! #{player.name} you have #{player.lives} lives left!"
         puts "\e[0m"
         show_scores
       else
         puts "\e[31m"
-        puts "Wrong! #{@players[player_key - 1][:name]} you have 1 life left!"
+        puts "Wrong! #{player.name} you have 1 life left!"
         puts "\e[0m"
     end
   end
 end
 
 def show_scores
-  puts "#{@players[0][:name]} your score is #{@players[0][:score]}."
-  puts "#{@players[1][:name]} your score is #{@players[1][:score]}.\n"
+  puts "#{@players[0].name} your score is #{@players[0].score}."
+  puts "#{@players[1].name} your score is #{@players[1].score}.\n"
 end
 
-def checkforloser(player_key)
-  if @players[player_key - 1][:lives] == 1 && !verify
-    puts "\e[31m\nWrong! #{@players[player_key - 1][:name]}, you have lost all your lives, game over!\e[0m"\
-          "\e[32m\n#{@players[0][:name]} your score was #{@players[0][:score]}, "\
-          "#{@players[1][:name]} your score was #{@players[1][:score]}.\n\e[0m"
+def endifloser(player)
+  if player.lives == 1 && !verify
+    puts "\e[31m\nWrong! #{player.name}, you have lost all your lives, game over!\e[0m"\
+          "\e[32m\n#{@players[0].name} your score was #{@players[0].score}, "\
+          "#{@players[1].name} your score was #{@players[1].score}.\n\e[0m"
     endmenu
   end
 end
 
-def set_names
-  puts "Player 1 please enter your name:"
-  @players[0][:name] += gets.chomp
-  puts "Player 2 please enter your name:"
-  @players[1][:name] += gets.chomp
-  @setnameflag = true
-end
-
 def resetlives
-  @players[0][:lives] = LIVES
-  @players[1][:lives] = LIVES
+  @players[0].lives = LIVES
+  @players[1].lives = LIVES
 end
 
 def endmenu
@@ -88,26 +78,8 @@ def endmenu
   input = gets.chomp.upcase
   if input == "Y"
     resetlives
-    start
+    run
   else
-    abort("\nThanks for playing!\n")
+    abort("\nThanks for playing!\n\n")
   end
 end
-
-def start
-  loop do
-    unless @setnameflag
-      set_names
-    end
-    @players.each do |player|
-      generate_question
-      prompt(player[:id])
-      verify
-      # byebug
-      checkforloser(player[:id])
-      scores(player[:id])
-    end
-  end
-end
-
-start
