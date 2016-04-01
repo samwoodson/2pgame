20786ba6c287f308556e6a5cf8296baf504686d1
samwoodson <samuelwoodson@gmail.com>
2016-04-01 00:52:54 +0000

    refactored

--- 2pgame.rb
+++ 2pgame.rb
@@ -1,70 +1,71 @@
-@PlayerOneScore = 0
-@PlayerTwoScore = 0
-@PlayerOneFails = 0
-@PlayerTwoFails = 0
-@CurrentPlayer = 1
 require 'byebug'
 require 'colorize'
+LIVES = 3
 
-def prompt
-  puts "Player #{@CurrentPlayer}: What is #{@first} plus #{@second}?"
+@players = [
+  {
+    id: 1,
+    lives: LIVES,
+    score: 0
+  },
+  {
+    id: 2,
+    lives: LIVES,
+    score: 0
+  }
+]
+
+def prompt(player_num)
+  puts "Player #{player_num}: What is #{@num1} plus #{@num2}?"
   @response = gets.chomp.to_i
 end
 
 def generate_question
-  @first = rand(1..20).to_i
-  @second = rand(1..20).to_i
-  @answer = (@first + @second)
+  @num1 = rand(1..20).to_i
+  @num2 = rand(1..20).to_i
+  @answer = (@num1 + @num2)
 end
 
 def verify
   @answer == @response
 end
 
-def scores
-  if @CurrentPlayer == 1 && verify
-    @PlayerOneScore += 1 
-    @CurrentPlayer += 1
-    puts "\e[32m"
-    puts "Correct!"
-    puts "\e[0m"
-  elsif @CurrentPlayer == 1
-    @PlayerOneFails += 1
-    @CurrentPlayer += 1
-    puts "\e[31m"
-    puts "Wrong! Your current score is #{@PlayerOneScore} with #{@PlayerOneFails} fails"
-    puts "\e[0m"
-  elsif @CurrentPlayer == 2 && verify
-    @PlayerTwoScore += 1
-    @CurrentPlayer -= 1
-    puts "\e[32m"
-    puts "Correct!"
-    puts "\e[0m"
-  elsif @CurrentPlayer == 2
-    @PlayerTwoFails += 1
-    @CurrentPlayer -= 1
-    puts "\e[31m"
-    puts "Wrong! Your current score is #{@PlayerTwoScore} with #{@PlayerTwoFails} fails"
-    puts "\e[0m"
+def scores(player_key)
+    if verify
+      @players[player_key - 1][:score] += 1
+      puts "\e[32m"
+      puts "Correct!"
+      puts "\e[0m"
+    else 
+      @players[player_key - 1][:lives] -= 1
+      puts "\e[31m"
+      puts "Wrong! You have #{@players[player_key - 1][:lives]} lives left!"
+      puts "\e[0m"
   end
 end
 
-def checkforloser
-  if @PlayerTwoFails == 3
-    abort("Player two, you have lost all your lives! Your score was #{@PlayerTwoScore} and the winner "\
-         "Player One has #{@PlayerOneScore}!")  
-  end
-  if @PlayerOneFails == 3
-    abort("Player 1, you have lost all your lives! Your score was #{@PlayerOneScore} and the winner "\
-         "Player 2 has a score of #{@PlayerTwoScore}!")
+def show_scores
+  puts "Player #{@players[0][:id]} your score is #{@players[0][:score]}."
+  puts "Player #{@players[1][:id]} your score is #{@players[1][:score]}."
+end
+
+def checkforloser(player_key)
+  if @players[player_key - 1][:lives] == 0
+    abort("Player #{@players[player_key - 1][:id]}, you have lost all your lives, game over! "\
+          "Player #{@players[0][:id]} your score was #{@players[0][:score]}, "\
+          "Player #{@players[1][:id]} your score was #{@players[1][:score]}.")
+    
   end
 end
 
 loop do
-  generate_question
-  prompt
-  verify
-  # byebug
-  scores
-  checkforloser
+  @players.each do |player|
+    generate_question
+    prompt(player[:id])
+    verify
+    # byebug
+    scores(player[:id])
+    checkforloser(player[:id])
+    show_scores
+  end
 end
